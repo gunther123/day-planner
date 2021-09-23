c8b0fcba7cd755e09107f37bb12c9cce3090327e
Steven Slocum <steven.t.slocum@gmail.com>
2021-09-23 10:22:46 -0500

    added array of workday hours and tasks assigned to each hour

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+//Add Variables for full Workday Schedule
+var scheduleBlocksArr = [
+    {
+        hour: "9:00 AM",
+        task: ""
+    },
+
+    {
+        hour: "10:00 AM",
+        task: ""
+    },
+
+    {
+        hour: "11:00 AM",
+        task: ""
+    },
+
+    {
+        hour: "12:00 PM",
+        task: ""
+    },
+
+    {
+        hour: "1:00 PM",
+        task: ""
+    },
+
+    {
+        hour: "2:00 PM",
+        task: ""
+    },
+
+    {
+        hour: "3:00 PM",
+        task: ""
+    },
+    {
+        hour: "4:00 PM",
+        task: ""
+    },
+
+    {
+        hour: "5:00 PM",
+        task: ""
+    },
+]
