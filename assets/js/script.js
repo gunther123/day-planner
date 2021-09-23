36a9334f9dbe02b073b971adcad6d143fe91d69e
Steven Slocum <steven.t.slocum@gmail.com>
2021-09-23 11:23:37 -0500

    templated schedule block styling and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,26 @@ var timeBlocksArr = [
         hour: "9:00 AM",
         task: ""
     },
-
     {
         hour: "10:00 AM",
         task: ""
     },
-
     {
         hour: "11:00 AM",
         task: ""
     },
-
     {
         hour: "12:00 PM",
         task: ""
     },
-
     {
         hour: "1:00 PM",
         task: ""
     },
-
     {
         hour: "2:00 PM",
         task: ""
     },
-
     {
         hour: "3:00 PM",
         task: ""
@@ -38,25 +32,25 @@ var timeBlocksArr = [
         hour: "4:00 PM",
         task: ""
     },
-
     {
         hour: "5:00 PM",
         task: ""
     },
 ];
 
-
 //Render the schedule to the page
-//Reference to where the container is in the html
-
 function renderPage(){
+    //Get all timeblocks and tasks
     const timeBlocks = getLocalStorage();
+    //Reference to where the container is in the html
     const timeContainer = $(".container");
 }
 
 
 function getLocalStorage(){
+    //Get Stored timeblocks and tasks
     timeBlocks = JSON.parse(localStorage.getItem("blocks"));
+    //If there are no timeblocks, get empty schedule array
     if (!timeBlocks){
         timeBlocks = timeBlocksArr;
     }
