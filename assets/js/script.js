38bbee3f7382c998312f5bda33fba5a933dc9a4b
Steven Slocum <stevesl23@hotmail.com>
2021-09-24 01:15:18 -0500

    added save task functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,20 +42,25 @@ var timeBlocksArr = [
 function renderPage(){
     //Get all timeblocks and tasks
     const timeBlocks = getLocalStorage();
+
     //Container for html elements
     let timeBlock = "";
+
     //Count variable for timeBlocks array iteration 
     let index = 0;
 
     timeBlocks.forEach((block) =>{
         //Reference to each hour in array
         const time = block.hour;
+
         //Reference to setting styling of text area
         const elClassColor = hasTimePassed(time);
+    
         //Reference to task for each hour from array
         const taskString = block.task;
+
         //Variable containing full HTML elements for each block
-        const blockHTMLTemplate = `<form class="row time-block"><div class="col-2 hour">${time}</div><textarea id="text-area-${index}" class="col-9 ${elClassColor}">${taskString}</textarea><button id="save-button${index}" dataAttr="save-button${index}" class="col-1 saveBtn"><i dataAttr="save-button${index}" class="far fa-save fa-lg"></i></button></form>`;
+        const blockHTMLTemplate = `<form class="row time-block"><div class="col-2 hour">${time}</div><textarea id="text-area-${index}" dataAttr="text-area" class="col-9 ${elClassColor}">${taskString}</textarea><button id="save-button${index}" dataAttr="save-button${index}" class="col-1 saveBtn"><i dataAttr="save-button${index}" class="far fa-save fa-lg"></i></button></form>`;
 
         timeBlock+= blockHTMLTemplate;
         index++;
@@ -64,6 +69,7 @@ function renderPage(){
     $("#container").html(timeBlock);
     //Get reference to all Button Elements
     const buttonGroup = document.getElementsByClassName("saveBtn");
+
     //Creating array from buttonGroup Elements
     var saveButtons = Array.prototype.slice.call(buttonGroup);
 
@@ -80,6 +86,7 @@ function renderPage(){
 function hasTimePassed(time){
     //Time of schedule block from array
     const timeParsed = moment(time, "hh:mm A").hours();
+
     //Current time
     let currentDateTime = moment().hours();
 
@@ -96,7 +103,8 @@ function hasTimePassed(time){
 
 function getLocalStorage(){
     //Get Stored timeblocks and tasks
-    timeBlocks = JSON.parse(localStorage.getItem("blocks"));
+    let timeBlocks = JSON.parse(localStorage.getItem("timeBlocks"));
+
     //If there are no timeblocks, get empty schedule array
     if (!timeBlocks){
         timeBlocks = timeBlocksArr;
@@ -104,6 +112,33 @@ function getLocalStorage(){
     return timeBlocks;
 };
 
+//Save TextArea text to local storage
+function saveTasks(e) {
+    //Prevent default refresh submit functionality
+    e.preventDefault();
+
+    //Get all previous tasks from local storage
+    const previousTasks = getLocalStorage();
+
+    //Get current button number
+    const buttonValueNumberString = e.srcElement
+      .getAttribute("dataAttr")
+      .split("")[11];
+
+    //Parse string to Int to pass into array
+    const buttonValueNumber = parseInt(buttonValueNumberString);
+
+    //Get the value of the text area
+    const taskString = $('#text-area-'+buttonValueNumberString).val();
+
+    //Add new task/updated task to array of current tasks
+    previousTasks[buttonValueNumber].task = taskString;
+
+    //Add new/updated tasks to local storage
+    localStorage.setItem("timeBlocks", JSON.stringify(previousTasks));
+  }
+
+//Get and set current date in header
 function getDate(){
     var currentDate = moment().format("dddd, MMMM Do");
     $("#current-day").text(currentDate);
